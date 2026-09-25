620d044dbd9103695823a44819405eae60d0e074
maximilian0-cmd <zomber9999@hotmail.com>
2026-09-25 11:44:06 -0300

    Add files via upload

--- sugerencias.py
+++ sugerencias.py
@@ -3,38 +3,22 @@ print("  sugerencias de peliculas  \n")
 print("============================\n")
 usuario=input("buen dia, cual es tu nombre? ")
 print("que queres mirar hoy, "+usuario+"?")
-nombre_pelicula="rapidos y furiosos"
-genero_pelicula="accion"
-anio_pelicula=2001
-rating_pelicula=6.8
-nombre_pelicula2="troya"
-genero_de_pelicula2="accion"
-anio_de_pelicula2=2004
-rating_pelicula2=7.4
-nombre_pelicula3="piratas del caribe"
-genero_de_pelicula3="accion"
-anio_de_pelicula3=2006
-rating_pelicula3=7.4
-nombre_pelicula4="¿Y dónde está el piloto?"   
-genero_pelicula4="Comedia"  
-anio_pelicula4=1980 
-rating_pelicula4=7.7 
-nombre_pelicula5="norbit"
-genero_pelicula5="comedia"
-anio_pelicula5=2007
-rating_pelicula5=4.3
+peliculas=[["rapidos y furiosos","accion",2001,6.8],["troya","accion",2004,7.4],["piratas del caribe","accion",2006,7.4],["¿Y dónde està el piloto?","comedia",2007,7.7],["smile","terror",2022,6.5],["norbit","comedia",2007,4.3],["jhon wick","accion",2014,7.5],["coco","animacion",2017,8.4],["up","animacion",2009,8.1],["el exorcista","terror",1973,8.1]]
 print("---- GÉNEROS-----")
 print("Acción")
 print("comedia")
+print("animacion")
+print("terror")
 genero_favorito=input("¿qué género te gusta? ")
 print ("Buscando Péliculas del Género " +genero_favorito)
-if (genero_pelicula==genero_favorito) :
-    print(nombre_pelicula)
-if (genero_de_pelicula2==genero_favorito):
-  print(nombre_pelicula2)
-  if (genero_de_pelicula3==genero_favorito):
-    print(nombre_pelicula3)
-    if (genero_pelicula4==genero_favorito):
-       print(nombre_pelicula4)   
-  if (genero_pelicula5==genero_favorito):
-     print(nombre_pelicula5)
+rating_favorito=float(input("¿cual es el rating minimo? "))
+encontrar_pelicula=False
+for pelicula in peliculas:
+     nombre=pelicula[0]
+     genero=pelicula[1]
+     rating=pelicula[3]
+     if (genero_favorito.lower()==genero.lower()) and (rating_favorito<rating):
+         print(nombre)
+         encontrar_pelicula=True
+if (not encontrar_pelicula):
+	print("No se ha encontrado ninguna pelìcula.") 
